c8b70ea2c246dd187bb25c0eac5e2f4098e95c49
faithgaiciumia <faithgaiciumia078@gmail.com>
2020-06-20 10:54:17 +0300

    Updates

---
+++ precache-manifest.8e24b3a1048ed43a5ee7dce22e09fc85.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b81d1c4bf6b363e2610f746d26ba6e",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "8abc333eae4d1534b063",
+    "url": "/resume/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6e2c08f1a73b022561d9",
+    "url": "/resume/static/js/2.1e77ab74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.1e77ab74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8abc333eae4d1534b063",
+    "url": "/resume/static/js/main.e4f481bf.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  },
+  {
+    "revision": "8b278b6e90f9d436b6d49a4443048951",
+    "url": "/resume/static/media/portfolioone.8b278b6e.PNG"
+  },
+  {
+    "revision": "e4582403314b477f58fc3b18b9b662b4",
+    "url": "/resume/static/media/portfoliothree.e4582403.jpg"
+  },
+  {
+    "revision": "5fa09852b9d18a180edc5bb76af69c01",
+    "url": "/resume/static/media/portfoliotwo.5fa09852.PNG"
+  }
+]);
