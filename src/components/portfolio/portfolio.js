b685803769aa55c00c4bc93fe4db2e4dde012745
faithgaiciumia <faithgaiciumia078@gmail.com>
2020-07-01 20:17:50 +0300

    a few updates

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import calc from "./portfolioone.PNG";
 import recipe from "./portfoliotwo.PNG";
+import quotes from "./tutorialone.PNG";
 import writing from "./portfoliothree.jpg";
 export default class Portfolio extends Component {
   render() {
@@ -115,6 +116,24 @@ export default class Portfolio extends Component {
               {/* item end */}
               <div className="row item">
                 <div className="twelve columns">
+                  <h3>Javascript Random Quotes Generator</h3>
+                  <p className="info">
+                    {/* UX Designer <span>•</span>{" "}
+                    <em className="date">March 2007 - February 2010</em> */}
+                  </p>
+                  <p>
+                    This is an app which generates and displays a new quote each time the user clicks on the new quote button.You could check out the full app {" "}
+                    <a href="https://faithgaiciumia.github.io/Javascript-Random-Quotes-Generator/">
+                      here
+                    </a>
+                  </p>
+                  <p>
+                    <img src={quotes} alt="quotes" />
+                  </p>
+                </div>
+              </div>
+              <div className="row item">
+                <div className="twelve columns">
                   <h3>Technical Articles</h3>
                   <p className="info">
                     {/* UX Designer <span>•</span>{" "}
@@ -125,7 +144,7 @@ export default class Portfolio extends Component {
                     where I share what I have learnt. I also use these articles
                     as a reminder for key concepts in the different languages,
                     libraries and frameworks I use. So far, I have published
-                    three technical articles; two on Medium and the other on
+                    three technical articles; two on Medium and the other two
                     Hashnode.
                     <a href="https://medium.com/@faithgaiciumia078/getting-started-with-reactjs-d95dc70dba8?source=your_stories_page---------------------------">
                       Article One
@@ -137,7 +156,8 @@ export default class Portfolio extends Component {
                     ,{" "}
                     <a href="https://hashnode.com/post/creating-a-simple-website-using-html-ckbfdt3uc01fbcls1lvx5id5e">
                       Article three
-                    </a>
+                    </a>,{" "}
+                    <a href="https://faithgaiciumia.hashnode.dev/creating-a-javascript-random-quote-generator-ckc11geu60002xbs10pqpdwuu">Article four</a>
                     .
                   </p>
                   <p>
